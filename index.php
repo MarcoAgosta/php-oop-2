149080253aa86bca6db0a8a2b17b10e2655222ae
MarcoAgosta <agostamarco@libero.it>
2023-01-16 17:04:46 +0100

    Pagina base completata

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ require_once "./classes/Product.php";
 
 $primoProdotto= new Product("Cani", 10, "Cesar al pollo", 3.5, "Cibo", "https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1662823176/products/pim/3065890114128_CesarSel_dell'ortoPolloVerdurer_8366_IT/cesar-cibo-umido-per-cani-con-pollo-e-verdure-selezioni-dell-orto");
 $secondoProdotto= new Product("Gatti", 4, "Bedsure cuccia", 20, "Cuccia", "https://m.media-amazon.com/images/I/81dIZOQXLqL._AC_SY355_.jpg");
+$terzoProdotto= new Product("Cani", 7, "Bite Toys", 5, "Gioco", "https://www.epocaitalpigeon.com/8982/bite-toys-gioco-osso-colorato-gomma-dura-per-cane-15-cm.jpg");
+$quartoProdotto= new Product("Gatti", 12, "Sheba al vitello", 3, "Cibo", "https://cdn.shopify.com/s/files/1/0288/2152/0466/products/cibo-umido-gatto-sheba-selezione-in-salsa-85-gr-vitello-tacchino.jpg?v=1634725379");
+$quintoProdotto= new Product("Cani", 3, "Cuccia in legno", 40, "Cuccia", "https://www.perilcane.it/4291-large_default/cuccia-chalet-doppia-da-esterno-in-legno-per-2-cani-taglia-media.jpg");
+$sestoProdotto= new Product("Gatti", 5, "Palestrina per gatti", 17.80, "Cuccia", "https://www.ulissequalityshop.com/wp-content/uploads/2017/04/camon-palestrina-gatti-gioco-appeso.jpg")
 ?>
 
 
@@ -21,7 +25,11 @@ $secondoProdotto= new Product("Gatti", 4, "Bedsure cuccia", 20, "Cuccia", "https
 </head>
 <body>
     <div class="container mt-5">
-        <div class="row">
+        <div class="text-center pb-4">
+            <h1>Articoli per Animali Domestici</h1>
+        </div>
+
+        <div class="row g-4 mb-3">
             <div class="col-4">
                 <div class="card">
                     <img src=<?php echo $primoProdotto->getImmagine() ?> class="card-img-top p-3" alt="...">
@@ -47,6 +55,66 @@ $secondoProdotto= new Product("Gatti", 4, "Bedsure cuccia", 20, "Cuccia", "https
                            echo $secondoProdotto->getIconTipo()?>
                         </div>
                         <p class="card-text">Prezzo: <?php echo $secondoProdotto->getPrezzo() ?>$</p>
+                        <a href="#" class="btn btn-primary">Compra</a>         
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card">
+                    <img src=<?php echo $terzoProdotto->getImmagine() ?> class="card-img-top p-3" alt="...">
+                    <div class="card-body" style="background-color: darkgray">
+                        <h5 class="card-title"><?php echo $terzoProdotto->getNome() ?></h5>
+                        <div class="icone">
+                           <?php echo $terzoProdotto->getIcon();  
+                           echo $terzoProdotto->getIconTipo()?>
+                        </div>
+                        <p class="card-text">Prezzo: <?php echo $terzoProdotto->getPrezzo() ?>$</p>
+                        <a href="#" class="btn btn-primary">Compra</a>         
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card">
+                    <img src=<?php echo $quartoProdotto->getImmagine() ?> class="card-img-top p-3" alt="...">
+                    <div class="card-body" style="background-color: darkgray">
+                        <h5 class="card-title"><?php echo $quartoProdotto->getNome() ?></h5>
+                        <div class="icone">
+                           <?php echo $quartoProdotto->getIcon();  
+                           echo $quartoProdotto->getIconTipo()?>
+                        </div>
+                        <p class="card-text">Prezzo: <?php echo $quartoProdotto->getPrezzo() ?>$</p>
+                        <a href="#" class="btn btn-primary">Compra</a>         
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card">
+                    <img src=<?php echo $quintoProdotto->getImmagine() ?> class="card-img-top p-3" alt="...">
+                    <div class="card-body" style="background-color: darkgray">
+                        <h5 class="card-title"><?php echo $quintoProdotto->getNome() ?></h5>
+                        <div class="icone">
+                           <?php echo $quintoProdotto->getIcon();  
+                           echo $quintoProdotto->getIconTipo()?>
+                        </div>
+                        <p class="card-text">Prezzo: <?php echo $quintoProdotto->getPrezzo() ?>$</p>
+                        <a href="#" class="btn btn-primary">Compra</a>         
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card">
+                    <img src=<?php echo $sestoProdotto->getImmagine() ?> class="card-img-top p-3" alt="...">
+                    <div class="card-body" style="background-color: darkgray">
+                        <h5 class="card-title"><?php echo $sestoProdotto->getNome() ?></h5>
+                        <div class="icone">
+                           <?php echo $sestoProdotto->getIcon();  
+                           echo $sestoProdotto->getIconTipo()?>
+                        </div>
+                        <p class="card-text">Prezzo: <?php echo $sestoProdotto->getPrezzo() ?>$</p>
                         <a href="#" class="btn btn-primary">Compra</a>         
                     </div>
                 </div>
